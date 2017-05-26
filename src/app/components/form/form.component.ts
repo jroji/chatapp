0c43101e44a945905a2096a2517b7dda44e476ec
Roji <roji@ZiMacMaster.local>
2017-05-26 20:15:53 +0200

    map updated

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {GMapsService} from "../../services/gmaps.service";
 
 @Component({
   selector: 'app-form',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  lat: 0;
-  lng: 0;
-  constructor() { }
+  lat: Number;
+  lng: Number;
+  newUser: FormGroup;
+  address: string;
+
+  constructor(private formBuilder: FormBuilder, private gmapsService: GMapsService) {
+
+    this.lat = 42;
+    this.lng = 7;
+    this.newUser = this.formBuilder.group({
+      user: this.formBuilder.group({
+        name: ['', [Validators.required]],
+        surname: ['', [Validators.required]],
+      }),
+      address: this.formBuilder.group({
+        latitude: [42],
+        longitude: [20],
+        address: ['']
+      })
+    });
+
+
+    const myFormValueChanges$ = this.newUser.controls['address'].valueChanges;
+
+    myFormValueChanges$.subscribe((data) => {
+
+      if(this.newUser.controls['address'].valid) {
+        this.lat = parseFloat(data.latitude);
+        this.lng = parseFloat(data.longitude);
+        this.gmapsService.getAddress(data.latitude, data.longitude).subscribe( address => {
+          if( address && address.results) {
+            this.address = address.results[0]['formatted_address'];
+          }
+        })
+      }
+
+    });
+
+  }
 
   ngOnInit() {
   }
